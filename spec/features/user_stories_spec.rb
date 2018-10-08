e21b702f223b247f916cd62b468a9844a6708abb
Lucien Najev <luciennajev@hotmail.com>
2018-10-08 12:04:42 +0100

    feature: feature and unit test for opening account, class created initializes with zero balance

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,3 +1,10 @@
-# As a bank customer
-# So that I can manage my finances
-# I want to be able to open an account
+describe 'User Stories' do
+  # As a bank customer
+  # So that I can manage my finances
+  # I want to be able to open an account
+  it 'so I can manage my finances, I want to open an account' do
+    account = Account.new
+    expect(account).to be_an_instance_of(Account)
+  end
+
+end
