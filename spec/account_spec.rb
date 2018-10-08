72337c980bca95cba6b86a30914e0a8be7265b85
Lucien Najev <luciennajev@hotmail.com>
2018-10-08 14:16:20 +0100

    feature: added an additional unit test for deposit to show it increments the balance and added a feature test for a user to check the balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -14,5 +14,10 @@ describe Account do
     it 'adds money to the account' do
       expect(account).to respond_to(:deposit).with(1).argument
     end
+
+    it 'adds money to the account balance' do
+      expect { account.deposit(1000) }.to change { account.balance }.by(1000)
+
+    end
   end
 end
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,19 +1,24 @@
 describe 'User Stories' do
+  let(:account) { Account.new }
+
   # As a bank customer
   # So that I can manage my finances
   # I want to be able to open an account
   it 'so I can manage my finances, I want to open an account' do
-    account = Account.new
     expect(account).to be_an_instance_of(Account)
   end
-
 
   # As a bank customer
   # So that I can build my savings
   # I want to be able to make a deposit to my bank account
   it 'so I can build my savings, I want to make a deposit' do
-    account = Account.new
     expect { account.deposit(1000) }.not_to raise_error
   end
 
+  # As a bank customer
+  # So that I can check how much is in my account
+  # I want to be able to check my current balance
+  it 'bank account owner can check the balance' do
+    expect(account.balance).to eq(0)
+  end
 end
