bea90fab5cc53ba04b6728af59ec57f9af41ac0e
Lucien Najev <luciennajev@hotmail.com>
2018-10-13 16:22:05 +0100

    refactor: added time stubbing to my feature tests so that date is fixed

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,11 +1,6 @@
 describe 'User Stories' do
   let(:account) { Account.new }
 
-  # before do
-  #   @fake_time = Time.now
-  #   Time.stub(:now) { @fake_time }
-  # end
-
   # As a bank customer
   # So that I can build my savings
   # I want to be able to make a deposit to my bank account
@@ -31,16 +26,18 @@ describe 'User Stories' do
     account.deposit(1000)
     account.deposit(2000)
     account.withdraw(500)
-    expect { account.withdraw(500) }.not_to raise_error
-    expect(account.balance).to eq(2000)
+    expect(account.balance).to eq(2500)
   end
 
   # As a bank customer
   # So that i can check my account history
   # I want to be able to get a bank account statement that includes cash flows and dates and the balance
   it 'so I can check my cashflows, I want to request a bank statement' do
+    allow(Time).to receive(:now).and_return(Time.mktime(2018, 10, 13))
     account.deposit(1000)
+    allow(Time).to receive(:now).and_return(Time.mktime(2018, 10, 13))
     account.deposit(2000)
+    allow(Time).to receive(:now).and_return(Time.mktime(2018, 10, 13))
     account.withdraw(500)
     # Ask about how i should go about mocking the Time.now element
     # allow(:date).to receive(Time.now).and_return(Time.parse('2018-10-10 15:08:34 +0100'))
